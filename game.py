1813f5c36378c122cc232824df06d541cbd44a05
John Maguire <contact@johnmaguire.me>
2016-02-14 18:56:48 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,328 @@
+import os
+import logging
+from random import shuffle
+from threading import Lock
+from collections import deque
+
+
+class InvalidMoveError(Exception):
+    pass
+
+
+class InvalidPickError(InvalidMoveError):
+    pass
+
+
+class InvalidChoiceError(InvalidMoveError):
+    pass
+
+
+class PlayerExistsError(Exception):
+    pass
+
+
+class NotEnoughPlayersError(Exception):
+    pass
+
+
+class Player(object):
+    WAITING = 1
+    CHOOSING = 2
+    PICKING = 3
+
+    def __init__(self, name, game):
+        self.state = self.WAITING
+        self.points = 0
+        self.hand = []
+
+        self.name = name
+        self.game = game
+
+    def draw(self, hand_size=10):
+        """Draws cards from the white deck until hand is full.
+
+        Keyword arguments:
+          hand_size -- Defaults to 10. Useful for draw 2, pick 3.
+        """
+        while len(self.hand) < hand_size and len(self.game.deck['white']):
+            self.hand.append(self.game.deck['white'].pop())
+
+    def choose(self, cards):
+        """Choose cards to play.
+
+        Raises:
+          InvalidMoveError -- If it's not the player's turn.
+          InvalidChoiceError -- An invalid card was chosen.
+        """
+        # Wrong time for player to choose
+        if self.state != Player.CHOOSING:
+            raise InvalidMoveError("Player isn't choosing cards now")
+
+        # Stage the players choices
+        choices = []
+
+        try:
+            for idx in cards:
+                choices.append(self.hand[int(idx)])
+        except Exception:
+            raise InvalidChoiceError("Choice %d was invalid" % idx)
+
+        choices = tuple(choices)
+
+        # Let exceptions escape. This is why we staged our choices
+        self.game.choose(self, choices)
+
+        # Remove the cards from the player's hand
+        for idx in cards:
+            self.hand.pop(int(idx))
+
+        # Draw cards for the player
+        self.draw()
+
+        self.state = Player.WAITING
+
+
+class Game(object):
+    STARTING = 1
+    WAITING_CHOICES = 2
+    WAITING_PICK = 3
+    OVER = 4
+
+    def __init__(self, max_points=5):
+        self.logger = logging.getLogger(__name__)
+
+        # Set max points to win
+        self.max_points = max_points
+
+        # Game storage
+        self.players = {}
+        self.choices = []
+        self.scores = []
+        self.lock = Lock()
+
+        # Reset game state
+        self.state = self.STARTING
+        self.picker = None
+        self.black_card = None
+        self.required_cards = 0
+        self.play_deque = deque()
+        self.deck = {
+            'white': self._load_deck('white'),
+            'black': self._load_deck('black'),
+        }
+
+    def _load_deck(self, name):
+        """Loads a deck by name (color)."""
+        filename = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "%s.txt" % name
+        )
+
+        with open(filename, 'r') as deck:
+            cards = deck.read().strip().split("\n")
+        shuffle(cards)
+
+        return cards
+
+    def ready(self):
+        """Starts the game and returns the picker and black card.
+
+        Raises:
+          InvalidMoveError -- If a game is already in progress.
+          NotEnoughPlayersError -- If there aren't at least 3 players.
+        """
+        if self.state != self.STARTING:
+            raise InvalidMoveError
+
+        if len(self.players) < 3:
+            raise NotEnoughPlayersError
+
+        self._prepare_round()
+
+        return
+
+    def _prepare_round(self):
+        """Starts a new round or ends the game if a win condition is hit.
+
+        Game ends if either deck is depleted, or someone has max points.
+
+        Sets the next picker, resets the choice list, sets player states,
+        draws a new black card, and sets the required number of chosen cards,
+        and sets the game state.
+        """
+        for _, player in self.players.items():
+            # Check if a player won
+            if player.points == self.max_points:
+                self._end_game()
+                return
+
+            # Draw cards
+            player.draw()
+
+        # Game ends if we are out of cards
+        if len(self.deck['white']) == 0 or len(self.deck['black']) == 0:
+            self._end_game()
+            return
+
+        # Set the picker and move them to the end of the queue
+        self.picker = self.play_deque.pop()
+        self.play_deque.appendleft(self.picker)
+
+        # Reset choices
+        self.choices = []
+
+        # Update player states
+        for player in self.players:
+            self.players[player].state = Player.CHOOSING
+        self.picker.state = Player.WAITING
+
+        # Choose a black card and determine the number of blanks
+        self.black_card = self.deck['black'].pop()
+
+        self.required_cards = self.black_card.count('%s')
+
+        # Some cards have no blanks. They require 1 white card
+        if self.required_cards == 0:
+            self.required_cards = 1
+
+        self.state = self.WAITING_CHOICES
+
+    def _end_game(self):
+        """Tallies results and ends the game."""
+        self._tally_scores()
+        self.state = self.OVER
+
+    def add_player(self, name):
+        """Adds a player to the game.
+
+        Raises:
+          InvalidMoveError -- If the game isn't starting.
+          PlayerExistsError -- If the player already exists.
+        """
+        name = str(name)
+
+        if self.state != self.STARTING:
+            raise InvalidMoveError
+
+        if name in self.players:
+            raise PlayerExistsError("%s is already playing" % name)
+
+        player = Player(name, self)
+        self.players[name] = player
+
+        # Add player to the play queue and re-shuffle it
+        self.play_deque.append(player)
+        shuffle(self.play_deque)
+
+    def remove_player(self, name):
+        """Removes a player from the game.
+
+        Raises:
+          KeyError -- If the player doesn't exist.
+        """
+        name = str(name)
+
+        self.play_deque.remove(self.players[name])
+        del self.players[name]
+
+        # Check if we don't have enough players now
+        if self.state != self.STARTING and len(self.players) < 3:
+            self._end_game()
+            return
+
+        # If we were waiting for this player to play, move on with the game
+        if (self.state == self.WAITING_CHOICES and
+                len(self.players) - 1 == len(self.choices)):
+            self._prepare_picks()
+
+        # If this player was supposed to pick, skip the round
+        elif self.state != self.STARTING and self.picker.name == name:
+            self._prepare_round()
+
+    def _prepare_picks(self):
+        """Prepares picks.
+
+        Shuffles the player choices and sets game state.
+        """
+        # Create an shuffled list of player, choice tuples
+        shuffle(self.choices)
+
+        self.picker.state = Player.PICKING
+        self.state = self.WAITING_PICK
+
+    def _tally_scores(self):
+        # Tally up new scores
+        players = sorted(self.players.items(), key=lambda p: p[1].points)
+        players.reverse()
+
+        self.scores = players
+
+    def pick(self, choice):
+        """Pick a round winner.
+
+        Raises:
+          InvalidMoveError -- If the game isn't waiting for a pick.
+          IndexError -- If the choice is invalid.
+        """
+        if self.state != self.WAITING_PICK:
+            raise InvalidMoveError("Wrong time to pick a winner")
+
+        try:
+            pick = self.choices[int(choice)]
+        except IndexError:
+            raise InvalidPickError("%d wasn't an option" % choice)
+
+        # Give the winner points
+        pick[0].points += 1
+
+        # Update scores
+        self._tally_scores()
+
+        # Start the next round
+        self._prepare_round()
+
+        return pick
+
+    def choose(self, player, cards):
+        """Chooses cards from a player's hand to play.
+
+        If the choices are made successfully, new cards will be drawn and the
+        new hand will be returned.
+
+        Raises:
+          InvalidMoveError -- If this is the wrong time to play.
+          ValueError -- If the wrong number of cards are played.
+        Returns:
+          list -- A copy of the player's hand.
+        """
+        # Wrong time to play a card
+        if self.state != self.WAITING_CHOICES:
+            raise InvalidMoveError("Wrong time to play cards")
+
+        # Not the right amount of cards
+        if len(cards) != self.required_cards:
+            raise ValueError("%d cards required, %d chosen" %
+                             (self.required_cards, len(cards)))
+
+        # Fill in blanks if there are any
+        choice = ''
+        if '%s' not in self.black_card:
+            choice = cards[0]
+        elif self.black_card.count('%s') == 1:
+            choice = self.black_card % cards[0]
+        elif self.black_card.count('%s') >= 2:
+            choice = self.black_card % cards
+
+        # Save player choices
+        self.choices.append((player, choice))
+
+        # If all players have made their choices, change the game state
+        if len(self.players) - 1 == len(self.choices):
+            self._prepare_picks()
+
+    def close(self):
+        # Prevent cyclic references during GC
+        del self.picker
+        self.deck.clear()
+        self.players.clear()
+        self.play_deque.clear()
---
+++ plugin.py
@@ -0,0 +1,305 @@
+import logging
+from cardinal.decorators import command, event, help
+
+from game import (Game, Player,
+                  InvalidMoveError, InvalidPickError, InvalidChoiceError,
+                  PlayerExistsError, NotEnoughPlayersError)
+
+
+class CAHPlugin(object):
+    def __init__(self, cardinal, config):
+        self.logger = logging.getLogger(__name__)
+        self.cardinal = cardinal
+
+        self.games = {}
+        self.channels = []
+
+        if 'channels' not in config:
+            return
+
+        self.channels = [str(channel) for channel in config['channels']]
+
+    @command('play')
+    @help("Joins or starts a new Cardinals Against Humanity game")
+    @help(".play")
+    def play(self, cardinal, user, channel, msg):
+        nick = user.group(1)
+
+        # Check if CAH is allowed here
+        if channel not in self.channels:
+            return cardinal.sendMsg(nick,
+                                    "Sorry, CAH isn't allowed here. Try "
+                                    "one of these channels instead: %s" %
+                                    ' '.join(self.channels))
+
+        # Attempt to get the game
+        try:
+            game = self.games[channel]
+        # Create a new game and add the player to it
+        except KeyError:
+            self.games[channel] = Game()
+            self.games[channel].add_player(nick)
+
+            cardinal.sendMsg(
+                channel, "A new game of Cardinals Against Humanity has been "
+                         "created. You've automatically been joined. Other "
+                         "players can use .play to join.")
+            cardinal.sendMsg(
+                channel, "You can use .ready to start the game. Have fun!")
+            return
+
+        try:
+            game.add_player(nick)
+        except InvalidMoveError:
+            cardinal.sendMsg(channel, "The game is already in progress.")
+            return
+        except PlayerExistsError:
+            cardinal.sendMsg(channel, "You're already playing :)")
+            return
+
+        cardinal.sendMsg(channel, "%s has joined the game." % nick)
+        cardinal.sendMsg(channel, "Players: %s" % ', '.join([
+            player for player in game.players
+        ]))
+
+    @command('ready')
+    @help("Begin the CAH game!")
+    @help('.ready')
+    def ready(self, cardinal, user, channel, msg):
+        try:
+            game = self.games[channel]
+            game.ready()
+        except InvalidMoveError:
+            cardinal.sendMsg(channel, "The game has already begun.")
+            return
+        except KeyError:
+            cardinal.sendMsg(channel, "No game in progress. "
+                                      "Type .cah to start one!")
+        except NotEnoughPlayersError:
+            cardinal.sendMsg(channel, "Not enough players to begin the game!")
+            return
+
+        cardinal.sendMsg(channel, "The game has begun! We will be playing "
+                                  "until someone earns %d points." %
+                                  game.max_points)
+
+        self.show_black_card(channel)
+        self.show_hands(channel)
+
+    @command('choose')
+    @help("Choose cards to play")
+    @help(".choose <choice [choice, [..]]>")
+    def choose(self, cardinal, user, channel, msg):
+        """Play a card or card combination"""
+        nick = user.group(1)
+
+        # Get the choices
+        choices = msg.strip().split(' ')[1:]
+
+        # If only one game is running, let them use PM
+        if nick == channel and len(self.games) == 1:
+            channel = self.games.keys()[0]
+        elif nick == channel:
+            cardinal.sendMsg(channel, "Use .choose in the game channel!")
+            return
+
+        try:
+            game = self.games[channel]
+        # Ignore invalid channel
+        except KeyError:
+            return
+
+        try:
+            player = game.players[nick]
+        # Ignore invalid player
+        except KeyError:
+            return
+
+        if game.state == Game.STARTING:
+            return
+
+        if player.state == Player.WAITING:
+            cardinal.sendMsg(channel, "Wait your turn please.")
+            return
+
+        if player.state == Player.CHOOSING:
+            # Make player choice
+            try:
+                player.choose(choices)
+            except InvalidChoiceError:
+                cardinal.sendMsg(channel, "You must choose %d cards." %
+                                          game.required_cards)
+                return
+
+            # Check if game transitioned
+            if game.state == Game.WAITING_PICK:
+                self.show_choices(channel)
+            else:
+                choosing = []
+                for _, p in game.players.items():
+                    if p.state == Player.CHOOSING:
+                        choosing.append(p.name)
+
+                cardinal.sendMsg(channel,
+                                 "%s has chosen. Still choosing: %s" %
+                                 (player.name, ', '.join(choosing)))
+
+        elif player.state == Player.PICKING:
+            # Make sure they aren't flubbing the command
+            if len(choices) > 1:
+                cardinal.sendMsg(
+                    channel,
+                    "You're picking a winner, so you can only make one choice."
+                )
+                return
+
+            # Make player pick
+            try:
+                win = game.pick(choices[0])
+                winner = win[0]
+                card = win[1]
+            except InvalidPickError:
+                cardinal.sendMsg(channel, "Please pick one of the options.")
+                return
+
+            cardinal.sendMsg(channel,
+                             "%s won the round with '%s' Congrats! You "
+                             "have %d point(s)." % (winner.name, card,
+                                                    winner.points))
+
+            # Check if game transitioned, and show new choices
+            if game.state == Game.WAITING_CHOICES:
+                self.show_black_card(channel)
+                self.show_hands(channel)
+
+        if game.state == Game.OVER:
+            self.finish_game(channel)
+
+    @event('irc.kick')
+    def _kicked(self, cardinal, kicker, channel, kicked, _):
+        """Remove kicked players from the game"""
+        try:
+            self.remove_player(channel, kicked)
+        except KeyError:
+            return
+
+    @event('irc.part')
+    def _left(self, cardinal, leaver, channel, _):
+        """Remove players who part from the game"""
+        try:
+            self.remove_player(channel, leaver.group(1))
+        except KeyError:
+            return
+
+    @event('irc.quit')
+    def _quit(self, cardinal, quitter, _):
+        """Remove players who quit from the game"""
+        for channel, _ in self.games.items():
+            try:
+                self.remove_player(channel, quitter.group(1))
+            except KeyError:
+                return
+
+    def remove_player(self, channel, player):
+        """Removes a player from a channel's game.
+
+        Raises:
+          KeyError -- If a game or player doesn't exist.
+        """
+        game = self.games[channel]
+
+        initial_state = game.state
+
+        self.games[channel].remove_player(player)
+        self.cardinal.sendMsg(channel, "%s left the game!" % player)
+
+        if (initial_state == Game.WAITING_PICK and
+                game.state == Game.WAITING_CHOICES):
+            self.cardinal.sendMsg(channel, "Round skipped.")
+            self.show_black_card(channel)
+            self.show_hands(channel)
+
+        elif (initial_state == Game.WAITING_CHOICES and
+              game.state == Game.WAITING_PICK):
+            self.show_choices(channel)
+
+        elif game.state == Game.OVER:
+            self.cardinal.sendMsg(channel, "The game has ended by default.")
+            self.finish_game(channel)
+
+    def show_hands(self, channel):
+        game = self.games[channel]
+
+        for nick, player in game.players.items():
+            if player.state == Player.WAITING:
+                self.cardinal.sendMsg(nick, "You are picking this round.")
+                continue
+
+            hand = []
+
+            # Instructions
+            syntax = ['<choice>' for _ in xrange(game.required_cards)]
+            syntax.insert(0, '.choose')
+            self.cardinal.sendMsg(nick, "Use %s to make your choice(s)." %
+                                        (' '.join(syntax)))
+
+            # Hand
+            for idx, card in enumerate(player.hand):
+                hand.append("[%d] %s" % (idx, card))
+            self.cardinal.sendMsg(nick, "Hand: %s" % ' '.join(hand))
+
+            # Prompt (black card)
+            self.cardinal.sendMsg(nick, "Black card: %s" %
+                                        game.black_card.replace('%s', '____'))
+
+    def show_black_card(self, channel):
+        game = self.games[channel]
+
+        self.cardinal.sendMsg(channel, "Black card: %s | Player picking: %s" %
+                                       (game.black_card.replace('%s', '____'),
+                                        game.picker.name))
+
+    def show_choices(self, channel):
+        game = self.games[channel]
+
+        # No blanks, show prompt
+        if '%s' not in game.black_card:
+            self.cardinal.sendMsg(channel, "%s" % game.black_card)
+
+        for idx, choice in enumerate(game.choices):
+            # Send the option
+            self.cardinal.sendMsg(channel, "  %d: %s" %
+                                           (idx, choice[1]))
+
+        self.cardinal.sendMsg(channel, "%s: Make your choice with .choose!" %
+                                       game.picker.name)
+
+    def finish_game(self, channel):
+        game = self.games[channel]
+
+        self.cardinal.sendMsg(channel, "Good game! You may use .play to start "
+                                       "a new one.")
+
+        standing = 0
+        for name, player in game.scores:
+            standing += 1
+            self.cardinal.sendMsg(channel,
+                                  "%d. %s - %d points" %
+                                  (standing, name, player.points))
+
+        # Close the game cleanly
+        game.close()
+        del self.games[channel]
+
+        return
+
+    def close(self, cardinal):
+        # TODO: Kill off running timers
+
+        for channel in self.games:
+            self.games[channel].close()
+        self.games.clear()
+
+
+def setup(cardinal, config):
+    return CAHPlugin(cardinal, config)
